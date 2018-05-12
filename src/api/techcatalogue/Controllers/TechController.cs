43bdec627a712349263713ca95ee26e2c8f6efc5
Nick Ramirez <nickram44@gmail.com>
2018-05-12 12:21:22 -0400

    Added grafana data source and updated API controller.

--- src/api/techcatalogue/Controllers/TechController.cs
+++ src/api/techcatalogue/Controllers/TechController.cs
@@ -4,23 +4,43 @@ using JustEat.StatsD;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("api/[controller]")]
-public class TechController : Controller 
+public class TechController : ControllerBase
 {
     private IStatsDPublisher statsPublisher;
+
+    private IList<string> technologies;
 
     public TechController(IStatsDPublisher statsPublisher)
     {
         this.statsPublisher = statsPublisher;
+        this.technologies = new List<string>() 
+        {
+            "Visual Studio Team Services",
+            "Gitlab",
+            "Jenkins"
+        };
     }
     
     [HttpGet]
     public IEnumerable<string> Get()
     {
-        this.statsPublisher.Increment("tech");
-        
-        return new List<string>() 
+        this.statsPublisher.Increment("techcatalogue.tech.get");
+
+        return this.technologies;
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(200, Type = typeof(string))]
+    [ProducesResponseType(404)]
+    public IActionResult Get(int id)
+    {
+        this.statsPublisher.Increment("techcatalogue.tech.get.id");
+
+        if (id < this.technologies.Count) 
         {
-            "Puppet"
-        };
+            return Ok(this.technologies[id]);
+        }
+
+        return NotFound();
     }
 }
