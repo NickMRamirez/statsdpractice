9d9455d7a6138087a2c5e8fe1d246146c76b6ca9
Nick Ramirez <nickram44@gmail.com>
2018-05-21 00:27:13 -0400

    Added web project

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Statsd Practice
 
 Practice using statsd
+
+## Goals
+
+* Is this feature being used?
+* How fast is this function?
+* 
---
+++ src/web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web.Models;
+
+namespace web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("home/register/{level=starter}")]
+        public IActionResult Register(string level) 
+        {
+            
+            
+            return View("~/Views/Home/register.cshtml");
+        }
+    }
+}
